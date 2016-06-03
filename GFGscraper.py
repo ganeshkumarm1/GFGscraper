d54b5a12373dd0e1fd3128551eeec037473730f6
GaneshmKumar <ganeshkumar.m1996@gmail.com>
2016-06-03 13:28:36 +0530

    Changed Algo and Ds

--- GFGscraper.py
+++ GFGscraper.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import prettytable
 import requests
@@ -7,9 +8,6 @@ import urllib2
 class geeksforgeeks():
     menu = []
     chapters = []
-    #numbers = []
-    #names = []
-    #links = []
     url = ''
     option = -1
 
@@ -18,8 +16,8 @@ class geeksforgeeks():
             print "---------------"
             self.url = 'http://www.geeksforgeeks.org/'
 
-    def getMenu(self):
-        notinclue = ['Home', 'Contribute', 'Ask Q', 'AndroidApp', 'Jobs', 'GBlog', 'MCQ', 'Misc' , 'O/P', 'C/C++']
+    def Menu(self):
+        notinclue = ['Home', 'Contribute', 'Ask Q', 'AndroidApp', 'Jobs', 'GBlog', 'MCQ', 'Misc' , 'O/P', 'C/C++', 'Job Seekers', 'Employers', 'Advertise with us', 'Internship']
         #Extracting the contents of the url
         r = requests.get(self.url)
         soup = BeautifulSoup(r.text, "html.parser")
@@ -30,28 +28,34 @@ class geeksforgeeks():
         for atag in divs:
             a = atag.findAll('a')
 
-        #Zipping links with names and numbers
+
+        del a[2:16]
+        del a[3:15]
         i = 1
         l = []
-        menuTable = prettytable.PrettyTable(["Option", "Topic"])
+        self.menuTable = prettytable.PrettyTable(["Option", "Topic"])
+
 
         for atag in a:
             if atag.text not in notinclue:
                 l.append(i)
                 l.append(str(atag.text))
-                menuTable.add_row(l)
+                self.menuTable.add_row(l)
                 l.append(str(atag.get('href')))
                 self.menu.append(l)
                 i = i + 1
                 l = []
-        #del self.menu[0]
-        print menuTable
-        self.option = self.selectOption(menuTable, 0)
-        self.directory = self.menu[option - 1][1]
+        self.getMenu()
+
+    def getMenu(self):
+        print self.menuTable
+        self.option = -1
+        while(self.option < 0 or self.option > len(self.menu)):
+            self.option = input("Enter an option to download: ")
+
+        self.directory = self.menu[self.option - 1][1]
         if not os.path.exists(self.directory):
             os.makedirs(self.directory)
-        #print self.option
-        #self.menu = []
         self.getTopic()
 
     def getTopic(self):
@@ -70,8 +74,11 @@ class geeksforgeeks():
             divs = soup.findAll('div', {'class': 'site-content'})
         for atag in divs:
             a = atag.findAll('a')
-        #print a
-        #print self.links
+
+        if(self.option == 1):
+            del a[0:17]
+        elif(self.option == 2):
+            del a[0:13]
         chapterTable = prettytable.PrettyTable(["Option", "Topic"])
         i = 1
         l = []
@@ -87,36 +94,38 @@ class geeksforgeeks():
         option = -1
         while(option < 0 or option > len(self.chapters)):
             option = input("Enter an option to download (Enter '0' to download all): ")
-        #print self.chapters[option - 1][2]
-        #for x in self.chapters:
-            #print x.split('/')[-2]
-            #print len(self.chapters)
 
+        print "Downloading file(s) ........"
         if(option == 0):
             for x in self.chapters:
                 rq = urllib2.Request(x[2])
                 res = urllib2.urlopen(rq)
                 f = open(self.directory + "/" + x[1] + ".html", 'wb')
                 f.write(res.read())
+                print "File saved to " +  self.directory + "/" + x[1] + ".html"
                 f.close()
         else:
             rq = self.chapters[option - 1][2]
             res = urllib2.urlopen(rq)
             f = open(self.directory + "/" + self.chapters[option - 1][1] + ".html", 'wb')
             f.write(res.read())
+            print "File saved to " +  self.directory + "/" + self.chapters[option - 1][1] + ".html"
             f.close()
 
-    def selectOption(self, Table, minValue):
-        option = input("Enter any option: ")
-        if option <= minValue:
-            print "Invalid Option"
-            self.selectOption(Table)
-        elif option > len(self.menu):
-            print "Invalid Option"
-            self.selectOption(Table)
+        self.chapters = []
+        opt = -1
+        print "1. Menu"
+        print "2. Topics"
+        print "3. Exit"
+        while(opt != 1 and opt != 2 and opt != 3):
+            opt = input("Enter your option: ")
+        if(opt == 1):
+            self.getMenu()
+        elif(opt == 2):
+            self.getTopic()
         else:
-            return option
+            sys.exit();
+
 
 gfg = geeksforgeeks()
-gfg.getMenu()
-#gfg.getTopic()
+gfg.Menu()
